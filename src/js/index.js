c1ad47f2ed93046dfec9101c5923473599f1be17
Laura Comte <comtelaurapro@gmail.com>
2023-07-03 10:11:39 +0200

    advanced

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,23 @@ const main = async () => {
   let input = document.getElementById("input");
   let button = document.getElementById("button");
   let body = document.getElementById("body");
+  let container = document.getElementById("container");
   let inputValue = "";
   let newTodo = null;
-  let finalData = "";
+  let finalData = [];
+  let dataArray = [];
+
+  const getLocalStorage = () => {
+    const storage = localStorage.getItem("data");
+    if (!storage) {
+      return null;
+    }
+    let previousData = JSON.parse(storage);
+    // return previousData;
+    finalData.push(...previousData);
+  };
+
+  getLocalStorage();
 
   let sendIt = async () => {
     inputValue = input.value;
@@ -18,31 +32,60 @@ const main = async () => {
         userId: 1,
         id: 1,
         title: newTodo.title,
-        completed: false,
+        completed: newTodo.completed,
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
     });
     const data = await toDo.json();
-    const dataJson = JSON.stringify(data); //transformation du fichier en json pour le stocker localement.
+    finalData.push(data);
+    const dataJson = JSON.stringify(finalData); //transformation du fichier en json pour le stocker localement.
     localStorage.setItem("data", dataJson);
-    const dataFromLocalS = localStorage.getItem("data");
-    finalData = JSON.parse(dataFromLocalS);
+    // const dataFromLocalS = localStorage.getItem("data");
+    // finalData = JSON.parse(dataFromLocalS);
     // recuperation et transformation de la donnee json vers le format js depuis le local storage
-    creatediv();
+    container.innerHTML = "";
+    finalData.forEach((element) => {
+      creatediv(element);
+    });
 
     console.log(finalData);
   };
 
   button.addEventListener("click", sendIt);
 
-  let creatediv = () => {
+  let creatediv = (element) => {
     let div = document.createElement("div");
+    let inputCheckbox = document.createElement("input");
+    inputCheckbox.id = "checkbox";
+    inputCheckbox.value = "unchecked";
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.classList.add("inputCheckbox");
     div.classList.add("finalData");
-    div.append(finalData.title);
-    body.append(div);
+    div.append(inputCheckbox);
+    div.prepend(element.title);
+    container.append(div);
+    input.value = "";
+
+    let cross = () => {
+      if (inputCheckbox.value === "unchecked") {
+        element.completed = true;
+        inputCheckbox.value = "checked";
+        console.log(element.completed);
+      } else if (inputCheckbox.value === "checked") {
+        element.completed = false;
+        inputCheckbox.value = "unchecked";
+        console.log("false");
+      }
+    };
+
+    inputCheckbox.addEventListener("click", cross);
   };
+
+  finalData.forEach((element) => {
+    creatediv(element);
+  });
 };
 
 window.addEventListener("load", main);
